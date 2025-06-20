d2bc5c3b1b3cf213fab34210f42caab732c7120f
da847 <dahlia.aghaei3@gmail.com>
2025-06-20 00:38:06 -0400

    ARAZDESKTOP - Build.bat automatic

--- Build.bat
+++ Build.bat
@@ -1,4 +1,3 @@
-copy /Y "backend\.env.prod" "backend\.env"
 copy /Y "project-config.json" "frontend\src\project-config.json"
 start cmd /k "cd frontend && npm run build"
 pause
@@ -12,6 +11,4 @@ git add .
 set "COMMIT_MSG=%COMPUTERNAME% - %~nx0 automatic"
 git commit -m "%COMMIT_MSG%"
 git push
-
-copy /Y "backend\.env.dev" "backend\.env"
 pause
