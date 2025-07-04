1b68efbf46a49062e1316f49452c337fb7477b68
DA847 <dahlia.aghaei3@gmail.com>
2025-07-04 14:16:16 -0400

    ARAZDESKTOP - push_simple.bat automatic

--- push_simple.bat
+++ push_simple.bat
@@ -1,4 +1,5 @@
 :: ── NEW: capture exact Python package versions
+call "%~dp0venv\Scripts\activate.bat"
 python -m pip freeze > "backend\requirements.txt"
 git add .
 set "COMMIT_MSG=%COMPUTERNAME% - %~nx0 automatic"
