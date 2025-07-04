0c9560086029ed019a0dd01b844310b28fd23a33
DA847 <dahlia.aghaei3@gmail.com>
2025-07-04 14:05:41 -0400

    ARAZDESKTOP - Build.bat automatic

--- Build.bat
+++ Build.bat
@@ -1,7 +1,7 @@
 copy /Y "project-config.json" "frontend\src\project-config.json"
 start cmd /k "cd frontend && npm run build"
 pause
-
+call "%~dp0venv\Scripts\activate.bat"
 :: ── NEW: capture exact Python package versions
 python -m pip freeze > "backend\requirements.txt"
 
