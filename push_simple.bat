fb3358fe2363cee96da544aef26aa517d14fce1a
DA847 <dahlia.aghaei3@gmail.com>
2025-12-28 16:56:51 -0500

    ARAZDESKTOP - Build.bat automatic

--- push_simple.bat
+++
@@ -1,9 +0,0 @@
-:: ── NEW: capture exact Python package versions
-call "%~dp0venv\Scripts\activate.bat"
-python -m pip freeze > "backend\requirements.txt"
-robocopy "frontend\dist" "docs" /MIR
-git add .
-set "COMMIT_MSG=%COMPUTERNAME% - %~nx0 automatic"
-git commit -m "%COMMIT_MSG%"
-git push
-pause
